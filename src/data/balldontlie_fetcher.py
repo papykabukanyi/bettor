90b672b874927947c2fdffa8fa1576e97c15d35b
Papi <papykabukanyi@gmail.com>
2026-07-21 23:55:35 -0500

    1q2q2q

--- src/data/balldontlie_fetcher.py
+++
@@ -1,8 +0,0 @@
-﻿"""
-BallDontLie fetcher - disabled.
-This project covers MLB and Soccer only; BallDontLie (NBA) is not used.
-"""
-
-
-def populate_db(*args, **kwargs):
-    print("[balldontlie] skipped - MLB/Soccer only project")
